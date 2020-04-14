f7876fd7d8a51123300ecb14dd5b77156b31dba0
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-14 00:49:01 -0300

    Setup angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsy6r7oy79YZGD9jBXdKDWCkhW-P73qFQ",
+    authDomain: "pedrogram-9cdf3.firebaseapp.com",
+    databaseURL: "https://pedrogram-9cdf3.firebaseio.com",
+    projectId: "pedrogram-9cdf3",
+    storageBucket: "pedrogram-9cdf3.appspot.com",
+    messagingSenderId: "328115254882",
+    appId: "1:328115254882:web:e8d586523b589a154cc33b"
+  }
 };
