946b87a23e0aa409f81561920d40c0765b205815
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-14 22:56:07 -0300

    autenticação email e senha

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    AngularFireAuthModule
   ],
   declarations: [LoginPage]
 })
-export class LoginPageModule {}
+export class LoginPageModule { }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { sharedStylesheetJitUrl } from '@angular/compiler';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private toastCtrl: ToastController,
+    private fbAuth: AngularFireAuth
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+  }
 
   ngOnInit() {
   }
 
+  async submit() {
+    const loading = await this.loadingCtrl.create({ message: "Autenticando..." });
+    loading.present();
+
+    this
+      .fbAuth
+      .signInWithEmailAndPassword(
+        this.form.controls['email'].value,
+        this.form.controls['password'].value
+      ).then((data) => {
+        loading.dismiss();
+        //TODO: salvar o nome da imagem e depois ler
+        localStorage.setItem('pedrogram.user', JSON.stringify(new User('', data.user.email, '')));
+        this.navCtrl.navigateRoot('home');
+      })
+      .catch((error) => {
+        console.log(error);
+        loading.dismiss();
+        this.showMessage("Usuário ou senha inválidos");
+      });
+  }
+
+  async showMessage(message: string) {
+    const toast = await this.toastCtrl.create({ message: message, duration: 3000 });
+    toast.present();
+  }
+
+  async goToSignup() {
+    this.navCtrl.navigateForward('signup');
+  }
 }
