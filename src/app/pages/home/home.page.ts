d16278a97228f564c791c511200ff43d062a3e8b
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-15 00:03:02 -0300

    logout e foto do profile

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ToastController, NavController } from '@ionic/angular';
+import { ToastController, NavController, ActionSheetController } from '@ionic/angular';
 import { Post } from 'src/app/models/post.model';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +11,20 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public user: User = new User('', '', 'https://placehold/500');
   posts$: Observable<any[]>;
 
   constructor(
     private db: AngularFirestore,
     private toastCtrl: ToastController,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private actionCtrl: ActionSheetController
   ) {
     this.posts$ = this.db.collection('posts').valueChanges();
   }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem('pedrogram.user'));
     const img = localStorage.getItem('pedrogram.post');
     if (img) this.showMessage("Você tem uma publicação não salva");
   }
@@ -40,4 +44,25 @@ export class HomePage implements OnInit {
     toast.present();
   }
 
+  async showOptions() {
+    const actionSheet = await this.actionCtrl.create({
+      header: 'Opções',
+      buttons: [{
+        text: 'Logout',
+        role: 'destructive',
+        icon: 'power',
+        handler: () => {
+          localStorage.removeItem('pedrogram.user');
+          this.navCtrl.navigateRoot("/login");
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel'
+      }
+      ]
+    });
+
+    await actionSheet.present();
+  }
 }
